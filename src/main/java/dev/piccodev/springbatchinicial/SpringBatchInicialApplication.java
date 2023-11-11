d0b2f43248dcd166fc20d65d3ebfd2d7213bc788
Rafiere <rafapgbr@gmail.com>
2023-11-11 19:38:42 -0300

    feat: add initial files

---
+++ src/main/java/dev/piccodev/springbatchinicial/SpringBatchInicialApplication.java
@@ -0,0 +1,13 @@
+package dev.piccodev.springbatchinicial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchInicialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBatchInicialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/piccodev/springbatchinicial/SpringBatchInicialApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.piccodev.springbatchinicial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchInicialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
