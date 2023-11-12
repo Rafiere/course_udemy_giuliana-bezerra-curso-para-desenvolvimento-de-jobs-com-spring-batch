cb6af43403d1720990113f534e52f6bc63865521
Rafiere <rafapgbr@gmail.com>
2023-11-11 23:42:34 -0300

    refactor! refactor first job and its components names

--- src/main/java/dev/piccodev/springbatchinicial/config/BatchConfig.java
+++ src/main/java/dev/piccodev/springbatchinicial/config/BatchConfig.java
@@ -6,10 +6,13 @@ import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -26,23 +29,31 @@ public class BatchConfig {
 
     //Um job é uma tarefa que encapsulará uma rotina.
     @Bean
-    public Job imprimeOlaJob(){
+    public Job imprimeOlaJob2(){
         return jobBuilderFactory
-                .get("imprimeOlaJob")
-                .start(imprimeOlaStep())
+                .get("imprimeOlaJob2")
+                .start(imprimeOlaStep2())
+                .incrementer(new RunIdIncrementer()) //Esse incrementer adicionará um parâmetro para cada execução, permitindo que o job seja executado mais de uma vez.
+                                                     //Esse parâmetro não permitirá que o "job" retorne o processamento caso necessário.
                 .build();
     }
 
-    public Step imprimeOlaStep() {
+    public Step imprimeOlaStep2() {
 
-        return stepBuilderFactory.get("imprimeOlaStep")
-                .tasklet(new Tasklet() {
-                    @Override
-                    public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
-                        System.out.println("Olá, mundo!");
-                        return RepeatStatus.FINISHED; //Estamos dizendo que a tarefa terminará.
-                    }
-                }) //Aqui temos o tipo do step. Nesse caso, um "tasklet" é uma tarefa simples.
+        return stepBuilderFactory.get("imprimeOlaStep2")
+                .tasklet(imprimeOlaTasklet2(null)) //Aqui temos o tipo do step. Nesse caso, um "tasklet" é uma tarefa simples.
                 .build();
+    }
+
+    @StepScope //Para que possamos acessar o "jobParameters", temos que inserir esse método no contexto de step.
+    @Bean //Para que consigamos obter dados do contexto da aplicação.
+    public static Tasklet imprimeOlaTasklet2(@Value("#{jobParameters['nome']}") String nome) {
+        return new Tasklet() {
+            @Override
+            public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
+                System.out.println("Olá, " + nome);
+                return RepeatStatus.FINISHED; //Estamos dizendo que a tarefa terminará.
+            }
+        };
     }
 }
